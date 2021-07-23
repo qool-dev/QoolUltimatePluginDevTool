43aec3d6efcc51df61de9d818fdcc66d3b4b6441
fan87 <fan870774>
2021-07-23 16:07:01 +0800

    Initailized Project

---
+++ src/main/java/dev/qool/qoolultimateplugindevtool/QoolUltimatePluginDevTool.java
@@ -0,0 +1,16 @@
+package dev.qool.qoolultimateplugindevtool;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class QoolUltimatePluginDevTool extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
